97ef35cb185d5bd6502d6dbcf9537a70e55a9bc2
Liran-tal <taliran44@gmail.com>
2021-11-07 13:49:33 +0200

    exercises 1.1, 1.2

---
+++ 1/1.1.js
@@ -0,0 +1,21 @@
+let str = "Hello World!";
+let num = 100;
+let a_obj = {};
+let bulie = true;
+let nal = null;
+let asdfasd = undefined;
+let sym1 = Symbol();
+console.log("str is a " + typeof(str) + ", and its value is: " + str);
+console.log("num is a " + typeof(num) + ", and its value is: " + num);
+console.log("a_obj is a " + typeof(a_obj) + ", and its value is: " + a_obj);
+console.log("bulie is a " + typeof(bulie) + ", and its value is: " + bulie);
+console.log("nal is a " + typeof(nal) + ", and its value is: " + nal);
+console.log("asdfasd is a " + typeof(asdfasd) + ", and its value is: " + asdfasd);
+console.log("sym1 is a " + typeof(sym1));
+console.log("sym1 is a aaaaaaaaa");
+
+    
+    
+    
+    
+    
