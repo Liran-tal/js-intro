900ed7a7b7e0ba404641520f39a184c34858684a
Liran-tal <taliran44@gmail.com>
2021-11-08 11:55:44 +0200

    exercise 3.2

--- 3/3.2.js
+++ 3/3.2.js
@@ -14,18 +14,46 @@
 // */
 
 // // From function declarations to explicit and implicit return functions (one of each).
-// function welcome() {
-//     let welcome = 'Welcome to Appleseeds Bootcamp!';
-//     return welcome;
-// }
+function welcome() {
+    let welcome = 'Welcome to Appleseeds Bootcamp!';
+    return welcome;
+};
 
-// function power(a) {
-//     let myNumber = a;
-//     let result = Math.pow(myNumber, 2);
-//     return result;
-// }
+let welcomeEx = () => {
+    return 'Welcome to Appleseeds Bootcamp!';
+}
+
+let welcomeIm = () => 'Welcome to Appleseeds Bootcamp!';
+
+
+console.log(welcome());
+console.log(welcomeEx());
+console.log(welcomeIm());
+
+//****************** */
+
+function power(a) {
+    let myNumber = a;
+    let result = Math.pow(myNumber, 2);
+    return result;
+}
+
+let powerEx = a => {
+    let myNumber = a;
+    let result = Math.pow(myNumber, 2);
+    return result;
+}
+
+let powerIm = a => Math.pow(a, 2);
+
+console.log(power(2));
+console.log(powerEx(2));
+console.log(powerIm(2));
 
 // // From function expressions to IIFE functions.
-// const squareRoot = a => Math.sqrt(a);
+const squareRoot = a => Math.sqrt(a);
+(function (a = 2){ Math.sqrt(a)})();
 
-// const randomNumbers = (a, b) => Math.random() * (a - b) + b;
+
+const randomNumbers = (a, b) => Math.random() * (a - b) + b;
+(function (a = 50, b = -100){ Math.random() * (a - b) + b})();
