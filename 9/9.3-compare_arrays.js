84c0561b98aac8b457ab4954c09f7037036b2d7e
Liran-tal <taliran44@gmail.com>
2021-11-14 11:59:47 +0200

    Practice 9.3 - compare arrays

--- 9/9.3-compare_arrays.js
+++ 9/9.3-compare_arrays.js
@@ -1,8 +1,28 @@
-const food1 = ["Noodle", "Pasta", "Ice-cream", "Meat",
-"Cucumber", "Olives"];
-const food2 = ["Fries", "Ice-cream", "Pizza", "Olives",
-"Hamburgers"];
+const food1 = ["Noodle", "Pasta", "Ice-cream", "Meat", "Cucumber", "Olives"];
+const food2 = ["Fries", "Ice-cream", "Pizza", "Olives", "Hamburgers"];
 
 function compareArrays(array1, array2) {
-	
+	const inclusive_array = [];
+	let shorter = [];
+	let longer = [];
+
+	if (array1.length > array2.length){
+		shorter = array2;
+		longer = array1;
+	} else{
+		shorter = array1;
+		longer = array2;
+	}
+
+	// Implement includes()?
+	for (let i = 0; i < shorter.length; ++i){
+		for (let j = 0; j < longer.length; ++j){
+			if (shorter[i].localeCompare(longer[j]) === 0) {
+				inclusive_array.push(shorter[i]);
+			}
+		}
+	}
+	return (inclusive_array.length > 0 ? inclusive_array : false);
 }
+
+console.log("inclusive_array: ", compareArrays(food1, food2));
