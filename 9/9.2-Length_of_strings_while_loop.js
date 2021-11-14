2dc01a2f9fd4ea6a8d1cbc775bef8c5be7d50128
Liran-tal <taliran44@gmail.com>
2021-11-14 11:27:59 +0200

    Practice 9.2-Length_of_strings_while_loop edited with exercise 6.4

--- 9/9.2-Length_of_strings_while_loop.js
+++ 9/9.2-Length_of_strings_while_loop.js
@@ -1,3 +1,5 @@
+// With 6.3
+
 const array_of_strings = ["boo", "doooo", "hoo","ro"];
 
 function lengthOfStrings(array) {
@@ -11,3 +13,19 @@ function lengthOfStrings(array) {
 }
 
 console.log("lengthOfStrings(array_of_strings): ", lengthOfStrings(array_of_strings)); // -> [3, 5, 3, 2]
+
+
+// With 6.4
+
+const population = [79, 7.9, 790, 0.79];
+
+function populationPercentages(population) {
+	let index = 0;
+    const percentages = [];
+    while (index < population.length) {
+        percentages[index] = (population[index] * 100 / 7900);
+		++ index;
+     }
+    return percentages;
+}
+console.log(populationPercentages(population));
---
+++ 9/9.3-compare_arrays.js
@@ -0,0 +1,8 @@
+const food1 = ["Noodle", "Pasta", "Ice-cream", "Meat",
+"Cucumber", "Olives"];
+const food2 = ["Fries", "Ice-cream", "Pizza", "Olives",
+"Hamburgers"];
+
+function compareArrays(array1, array2) {
+	
+}
