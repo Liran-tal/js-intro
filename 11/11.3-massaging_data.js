5a42837fd94e35233ab7b3b9e8878bd5e789ed5c
Liran-tal <taliran44@gmail.com>
2021-11-15 14:24:17 +0200

    exercise 11.3.2 - massaging data

--- 11/11.3-massaging_data.js
+++ 11/11.3-massaging_data.js
@@ -50,4 +50,29 @@ function getNames(data) {
 	})
 }
 
-console.log('getNames(data): ', getNames(data));
+// console.log('getNames(data): ', getNames(data));
+
+
+//.2
+
+function birthYear(string) {
+	return Number(string.slice(-4));
+}
+
+// console.log('birthYear(string): ', birthYear("1-10-1977"));
+// console.log('typeOf(birthYear(string)): ', typeof(birthYear("1-10-1977")));
+
+function bornBefore1990(data) {
+	const older = [];
+	for (const obj of data) {
+		if (birthYear(obj.birthday) < 1990){
+			older.push(obj);
+		}
+	}
+	return older;
+}
+
+// console.log('bornBefore1990(data): ', bornBefore1990(data));
+
+
+//.3
