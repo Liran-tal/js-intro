db32cd1727a46b665beddc53724152b159c505c0
Liran-tal <taliran44@gmail.com>
2021-11-16 13:22:24 +0200

    exercise 14.1.2 - reduce

--- 14/14.1-reduce.js
+++ 14/14.1-reduce.js
@@ -6,8 +6,19 @@ const max = numbers.reduce((max_val, num) => {
 	return max_val < num ? num : max_val;
 })
 
-console.log('max value in numbers is: ', max);
+// console.log('max value in numbers is: ', max);
 
 
 //.2
 
+function sum_of_even (array){
+	let sum = numbers.reduce((sum, num) => {
+		return (num % 2 === 0) ? sum += num : sum;
+	});
+	
+	return numbers[0] % 2 !== 0 ? sum - numbers[0] : sum; 
+}
+console.log('sum value of even numbers in numbers is: ', sum_of_even(numbers));
+
+
+//.3
