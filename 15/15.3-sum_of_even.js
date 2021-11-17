7b77edb9ab624cd3e2ad68262723593fca91e4c5
Liran-tal <taliran44@gmail.com>
2021-11-17 14:16:16 +0200

    exercise 15.3 - sum_of_even_cells

---
+++ 15/15.3-sum_of_even.js
@@ -0,0 +1,14 @@
+// function getSumOfEven(arr) {
+// 	return arr[2] + arr[4] + arr[6] + arr[8] + arr[10];
+// }
+// getSumOfEven([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+function getSumOfEven(arr) {
+	return arr[1] + arr[3] + arr[5] + arr[7] + arr[9];
+}
+
+console.log("getSumOfEven([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]): ", getSumOfEven([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+
+
+//.1
+//I'm sorry, I really tried to use the debbuger, but that arr[10] is too obvious
