3571c31790796273accb9d8b4e65a657e1f91973
Liran-tal <taliran44@gmail.com>
2021-11-29 13:21:19 +0200

    21_2-font_size.

---
+++ 21/21_2-font_size.js
@@ -0,0 +1,35 @@
+"use strict";
+const increase_button = document.querySelector('.increase-btn');
+console.log(increase_button);
+const decrease_button = document.querySelector('.decrease-btn');
+console.log(decrease_button);
+
+increase_button.addEventListener('click', () => {
+	const example_text = document.querySelector('.example-text');
+	let font_size = 
+		window.getComputedStyle(example_text)
+		.getPropertyValue('font-size')
+		.match(/\d*\.?\d*/)[0];
+	
+	font_size = parseFloat(font_size);
+	if (font_size < 59) {
+		font_size += 2;
+	}
+	
+	example_text.style.fontSize = font_size + 'px';
+})
+
+decrease_button.addEventListener('click', () => {
+	const example_text = document.querySelector('.example-text');
+	let font_size = 
+		window.getComputedStyle(example_text)
+		.getPropertyValue('font-size')
+		.match(/\d*\.?\d*/)[0];
+	
+	font_size = parseFloat(font_size);
+	if (font_size > 7) {
+		font_size -= 2;
+	}
+	
+	example_text.style.fontSize = font_size + 'px';
+})
