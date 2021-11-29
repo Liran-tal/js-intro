5e606e7037d382f9bf1cfe9ed1e96c331aaca869
Liran-tal <taliran44@gmail.com>
2021-11-29 21:08:38 +0200

    22_5-smily

--- 21/22_5-smily.js
+++ 21/22_5-smily.js
@@ -2,12 +2,15 @@
 console.log('21.5');
 const smiley_array = ["😁", "😂", "😃", "😄", "😅", "😆", "😉", "😊", "😋", "😍"];
 
-const input_field = document.querySelector('.input-container input[type=text]');
+const input_field = document.querySelector('.input-container input[type=number]');
 const button = document.querySelector('.input-container input[type=button]');
 const output_field = document.querySelector('.output-container');
 
 button.addEventListener('click', () => {
 	const smiley_num = parseInt(input_field.value);
-	console.log(smiley_num);
+	output_field.innerText = '';
+	for (let i = 0; i < smiley_num; ++ i){
+		output_field.innerText += '  ' + smiley_array[Math.floor(Math.random() * smiley_array.length)] + '  ';
+	}
 });
 
