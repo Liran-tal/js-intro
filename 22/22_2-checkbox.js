d483e156a45a6588f006538c110811f901a1809a
Liran-tal <taliran44@gmail.com>
2021-11-30 14:15:29 +0200

    22_2-checkbox.

--- 22/22_2-checkbox.js
+++ 22/22_2-checkbox.js
@@ -1,2 +1,18 @@
 "use strict";
+console.log("*** 22.2 ***");
 
+const checkbox = document.querySelector('.checkbox');
+const img = document.querySelector('img');
+const checkbox_label = document.querySelector('.checkbox-label');
+console.log(checkbox_label);
+
+checkbox.addEventListener("change", () => {
+	img.classList.toggle('img-invisible');
+	if (checkbox.checked) {
+		console.log("checked");
+		checkbox_label.innerText = "Uncheck to make image disappear";
+	}else {
+		checkbox_label.innerText = "Check to make an image appear";
+	}
+	
+})
