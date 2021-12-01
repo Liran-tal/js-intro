bf6549ef237b027a185f03834c6ddc63334bd204
Liran-tal <taliran44@gmail.com>
2021-12-01 10:02:48 +0200

    22_4-race

--- 22/22_4-race_starter_files-master/race_starter_files-master/22_4-race.js
+++ 22/22_4-race_starter_files-master/race_starter_files-master/22_4-race.js
@@ -4,14 +4,31 @@ console.log('*** 22.4 ***');
 
 
 const track_length = document.querySelectorAll('#player1-race td').length;
-console.log(track_length);
 const player1_track = document.querySelector('#player1-race .active');
-console.log(player1_track);
 const player2_track = document.querySelector('#player2-race .active');
-console.log(player2_track);
 
-const restart_button = document.querySelector('.restart-btn');
-console.log(restart_button);
+function win(player) {
+	const win_screen = document.querySelector('win-popup')
+	const text_field = document.querySelector('.win-popup-text');
+	const restart_button = document.querySelector('.restart-btn');
+	const p1_color = 'red';
+	const p2_color = 'yellow';
+
+	if (player === 'p1') {
+		text_field.innerText = 'Player 1 Wins!!!'
+		text_field.style.color = p1_color;
+		restart_button.style.color = p1_color;
+		restart_button.style.borderColor = p1_color;
+	}
+	else {
+		text_field.innerText = 'Player 2 Wins!!!'
+		text_field.style.color = p2_color;
+		restart_button.style.color = p2_color;
+		restart_button.style.borderColor = p2_color;
+	}
+
+
+}
 
 function evaluateKey(event) {
 	const key = event.key;
@@ -25,7 +42,10 @@ function evaluateKey(event) {
 	}
 
 	if (p1_steps === track_length) {
-		
+		win ('p1');	
+	}
+	else if (p2_steps === track_length) {
+		win ('p2');
 	}
 }
 
