880cc98fee9574dafedf198cb573f86e5c12a957
Liran-tal <taliran44@gmail.com>
2021-12-01 09:43:25 +0200

    22_4-race html + css

--- 22/22_4-race_starter_files-master/race_starter_files-master/22_4-race.js
+++ 22/22_4-race_starter_files-master/race_starter_files-master/22_4-race.js
@@ -3,17 +3,29 @@
 console.log('*** 22.4 ***');
 
 
+const track_length = document.querySelectorAll('#player1-race td').length;
+console.log(track_length);
+const player1_track = document.querySelector('#player1-race .active');
+console.log(player1_track);
+const player2_track = document.querySelector('#player2-race .active');
+console.log(player2_track);
 
-const restart_button = document.querySelector('.restart-button');
+const restart_button = document.querySelector('.restart-btn');
+console.log(restart_button);
 
 function evaluateKey(event) {
-	const key = event.code;
-	
-	if (key === 39) {
-		letterCorrect();
+	const key = event.key;
+	let p1_steps = 1;
+	let p2_steps = 1;
+	if (key === 'ArrowRight') {
+		++ p1_steps;
 	}
 	if (key.toUpperCase() === 'D'){
-		letterWrong(key);
+		++ p2_steps;
+	}
+
+	if (p1_steps === track_length) {
+		
 	}
 }
 
