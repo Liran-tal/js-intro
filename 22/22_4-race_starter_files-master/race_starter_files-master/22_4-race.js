c018d23c510bf3f0be389101e4da8540396d3cdd
Liran-tal <taliran44@gmail.com>
2021-12-01 11:37:35 +0200

    22_4-race

--- 22/22_4-race_starter_files-master/race_starter_files-master/22_4-race.js
+++ 22/22_4-race_starter_files-master/race_starter_files-master/22_4-race.js
@@ -3,48 +3,57 @@
 console.log('*** 22.4 ***');
 
 
-const track_length = document.querySelectorAll('#player1-race td').length;
-const player1_track = document.querySelector('#player1-race .active');
-const player2_track = document.querySelector('#player2-race .active');
+const player1_track = document.querySelectorAll('#player1-race td'); 
+const player2_track = document.querySelectorAll('#player2-race td');
+const track_length = player1_track.length;
+const win_screen = document.querySelector('win-popup');
+const text_field = document.querySelector('.win-popup-text');
+const restart_button = document.querySelector('.restart-btn');
+
+function setWinScreenColors(color) {
+	text_field.style.color = color;
+	restart_button.style.color = color;
+	restart_button.style.borderColor = color;
+}
 
 function win(player) {
-	const win_screen = document.querySelector('win-popup')
-	const text_field = document.querySelector('.win-popup-text');
-	const restart_button = document.querySelector('.restart-btn');
 	const p1_color = 'red';
 	const p2_color = 'yellow';
 
 	if (player === 'p1') {
 		text_field.innerText = 'Player 1 Wins!!!'
-		text_field.style.color = p1_color;
-		restart_button.style.color = p1_color;
-		restart_button.style.borderColor = p1_color;
+		setWinScreenColors(p1_color);
 	}
 	else {
 		text_field.innerText = 'Player 2 Wins!!!'
-		text_field.style.color = p2_color;
-		restart_button.style.color = p2_color;
-		restart_button.style.borderColor = p2_color;
+		setWinScreenColors(p2_color);
 	}
 
+	win_screen.classList.remove('hide'); 
+}
 
+function moveIcon(current_pos) {
+	current_pos.classList.remove('active');
+	current_pos.nextElementSibling.classList.add('active');
 }
 
 function evaluateKey(event) {
 	const key = event.key;
-	let p1_steps = 1;
-	let p2_steps = 1;
+	const p1_current_place = document.querySelector('#player1-race .active');
+	const p1_finish = document.querySelector('#player1-race .finish');
+	const p2_current_place = document.querySelector('#player2-race .active')
+	const p2_finish = document.querySelector('#player2-race .finish')
 	if (key === 'ArrowRight') {
-		++ p1_steps;
+		moveIcon(p1_current_place);
 	}
 	if (key.toUpperCase() === 'D'){
-		++ p2_steps;
+		moveIcon(p2_current_place);
 	}
 
-	if (p1_steps === track_length) {
+	if (p1_current_place.classList.contains('.finish')) {
 		win ('p1');	
 	}
-	else if (p2_steps === track_length) {
+	else if (p2_current_place === p2_finish) {
 		win ('p2');
 	}
 }
