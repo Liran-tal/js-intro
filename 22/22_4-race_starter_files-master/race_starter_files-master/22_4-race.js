dc5401bd20cbbe73f0743bc9589aa1d6452bc6d3
Liran-tal <taliran44@gmail.com>
2021-12-01 12:17:04 +0200

    22_4-race - to review

--- 22/22_4-race_starter_files-master/race_starter_files-master/22_4-race.js
+++ 22/22_4-race_starter_files-master/race_starter_files-master/22_4-race.js
@@ -6,12 +6,13 @@ console.log('*** 22.4 ***');
 const player1_track = document.querySelectorAll('#player1-race td'); 
 const player2_track = document.querySelectorAll('#player2-race td');
 const track_length = player1_track.length;
-const win_screen = document.querySelector('win-popup');
+const win_screen = document.querySelector('.win-popup');
 const text_field = document.querySelector('.win-popup-text');
 const restart_button = document.querySelector('.restart-btn');
 
 function setWinScreenColors(color) {
 	text_field.style.color = color;
+	restart_button.style.fontBorder
 	restart_button.style.color = color;
 	restart_button.style.borderColor = color;
 }
@@ -19,7 +20,7 @@ function setWinScreenColors(color) {
 function win(player) {
 	const p1_color = 'red';
 	const p2_color = 'yellow';
-
+	console.log('hello from win');
 	if (player === 'p1') {
 		text_field.innerText = 'Player 1 Wins!!!'
 		setWinScreenColors(p1_color);
@@ -38,24 +39,22 @@ function moveIcon(current_pos) {
 }
 
 function evaluateKey(event) {
-	const key = event.key;
 	const p1_current_place = document.querySelector('#player1-race .active');
-	const p1_finish = document.querySelector('#player1-race .finish');
 	const p2_current_place = document.querySelector('#player2-race .active')
-	const p2_finish = document.querySelector('#player2-race .finish')
-	if (key === 'ArrowRight') {
+
+	if (p1_current_place.classList.contains('finish')) {
+		win('p1');	
+	}
+	else if (p2_current_place.classList.contains('finish')) {
+		win('p2');
+	}
+	if (event.key === 'ArrowRight') {
 		moveIcon(p1_current_place);
 	}
-	if (key.toUpperCase() === 'D'){
+	if (event.key.toUpperCase() === 'D'){
 		moveIcon(p2_current_place);
 	}
 
-	if (p1_current_place.classList.contains('.finish')) {
-		win ('p1');	
-	}
-	else if (p2_current_place === p2_finish) {
-		win ('p2');
-	}
 }
 
 window.addEventListener('keyup', evaluateKey);
